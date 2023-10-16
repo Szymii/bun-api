0b5596d81b2879a035466de7d29e89fabdcda804
szymii <szymimelzer9@gmail.com>
2023-10-16 15:13:46 +0000

    CI Fail test

--- index.test.ts
+++ index.test.ts
@@ -2,15 +2,15 @@ import { describe, expect, it } from "bun:test";
 import app from ".";
 
 describe("Routing is up", () => {
-  it("Should return 200 Response", async () => {
-    const req = new Request("http://localhost/");
-    const res = await app.fetch(req);
-    expect(res.status).toBe(200);
-  });
+	it("Should return 200 Response", async () => {
+		const req = new Request("http://localhost/");
+		const res = await app.fetch(req);
+		expect(res.status).toBe(200);
+	});
 
-  it("Should return 404 Response", async () => {
-    const req = new Request("http://localhost/notexistingroute");
-    const res = await app.fetch(req);
-    expect(res.status).toBe(404);
-  });
+	it("Should return 404 Response", async () => {
+		const req = new Request("http://localhost/notexistingroute");
+		const res = await app.fetch(req);
+		expect(res.status).toBe(500);
+	});
 });
