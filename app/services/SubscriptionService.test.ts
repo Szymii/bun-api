e4b79ec732be61735eb5612d1f938cbb1b9d8302
szymii <szymimelzer9@gmail.com>
2023-10-20 15:07:22 +0000

    Added test for Subscription service and Observable class

---
+++ app/services/SubscriptionService.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from "bun:test";
+import { createMockDB } from "../db/MockDB";
+import { SubscribersRepository } from "../repositories";
+import { getID } from "../utils/getId";
+import { SubscriptionService } from ".";
+
+describe("Subscription service", () => {
+	it("Add new subscriber to DB", async () => {
+		// arrange
+		const MockDB = createMockDB();
+		const emailRepo = new SubscribersRepository(MockDB);
+		const subService = new SubscriptionService(emailRepo);
+		const EMAIL = `${getID(10)}@example.com`;
+
+		// act
+		subService.addSubscriber(EMAIL);
+
+		// assert
+		expect(
+			!!emailRepo.getAllSubscribers().find(({ email }) => email === EMAIL),
+		).toBe(true);
+	});
+});
---
+++ app/utils/Observable.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it, mock } from "bun:test";
+import { Observable } from "./Observable";
+
+class FakeObservable extends Observable<number> {
+	triggerObservers(number: number) {
+		this.notify(number);
+	}
+}
+
+describe("Observable", () => {
+	it("Should triggers observers action", async () => {
+		// arrange
+		const observable = new FakeObservable();
+		const mockAction = mock((number: number) => number);
+
+		// act
+		observable.subscribe(mockAction);
+		observable.triggerObservers(11);
+
+		// assert
+		expect(mockAction).toHaveBeenCalled();
+	});
+
+	it("Should be possible to unsubscribe", async () => {
+		// arrange
+		const observable = new FakeObservable();
+		const mockAction = mock((number: number) => number);
+
+		// act
+		observable.subscribe(mockAction);
+		observable.unsubscribe(mockAction);
+		observable.triggerObservers(11);
+
+		// assert
+		expect(mockAction).not.toHaveBeenCalled();
+	});
+});
